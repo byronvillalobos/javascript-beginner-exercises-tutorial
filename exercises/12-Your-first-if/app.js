7e1c7f933a915edb0d42a077da16fe61f5a003ad
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-10 06:17:12 +0000

    changes

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,16 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+
+  
+  // First if statement 
+  if (total >= 50)  
+  console.log("We still have a bit of driving left to go"); 
+  
+  // Nested - if statement 
+  // Will only be executed if statement above 
+  // it is true 
+  if (total < 50)
+  console.log("We will be there in 5 minutes"); 
+  else
+  console.log("I'm parking. I'll see you right now"); 
