6d70eb45896176fd4faae9548b5fa23adc3ba303
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-11 03:36:19 +0000

    changes

--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,31 +1,25 @@
+var colorname = prompt('What color do you want?');
+var isAvailable = getColor(colorname);
+
+    
 function getColor(colorname)
 {
   switch (colorname) {
-  case rojo:
-	    	return true;  //returns true because the user picked an available color
-break;
+  case "rojo":
+  case "azul":
+  case "verde":
 
-  case azul:
-	    	return true;  //returns true because the user picked an available color
-
-break;
-  case verde:
+ 
 	    	return true;  //returns true because the user picked an available color
 
-break;
+
  default:
 	    	return false;  //returns false because the user picked an unavailable color
-}            
-	}
+  }
+}
 
-var colorname = prompt('What color do you want?');
-var isAvailable = getColor(colorname);
 
 if(isAvailable)
 	console.log('Good news! That color is available');
 else 
     console.log('We are sorry, that color is not available');
-    
-
-
-  
