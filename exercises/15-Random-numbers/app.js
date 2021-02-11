1da6394bc1eb1a49148947c00b1d3621590ba928
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-11 03:52:48 +0000

    changes

--- exercises/15-Random-numbers/app.js
+++ exercises/15-Random-numbers/app.js
@@ -2,9 +2,12 @@
 
 function getRandomInt()
 {
-	var randomNumber = Math.random();
+	var randomNumber = Math.floor((Math.random()*10) + 1); 
+
 	return randomNumber;
 }
 
 
 console.log(getRandomInt());
+
+
